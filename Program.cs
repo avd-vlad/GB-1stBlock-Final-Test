361d0c95bd14cf70bba5553842c922c6e63e3dac
Vladimir Avdeyev <avd-vlad@yandex.ru>
2022-10-12 00:36:54 +0300

    добавлен код

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿void ShowArr(string[] arr)
+{
+
+    Console.Write("[");
+    for(int i=0; i<arr.Length;i++)
+        if(i==arr.Length-1)
+            Console.Write($"{arr[i]} ]");
+        else
+            Console.Write($"{arr[i]}, ");
+    Console.WriteLine();
+}
+
+int CountStrings(string[] arr, int maxLen)
+{
+    int cnt=0;
+    for( int i = 0; i < arr.Length; i++)
+        if(arr[i].Length<=3)
+            cnt++;
+    return cnt;
+}
+
+
+int maxLen = 3;
+string [] inArr= new string[] {"hello","2","world",":-)"};
+
+int cnt = CountStrings(inArr, maxLen);
+
+string [] resArr= new string[cnt];
+int j = 0;
+
+for( int i = 0; i < inArr.Length; i++)
+{
+    if(inArr[i].Length<=3)
+    {
+        resArr[j] = inArr[i];
+        j++;
+    }
+}
+
+ShowArr(resArr);
